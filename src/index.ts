25e80c223070fd2e0790f5d3dc2778221bc0e4d1
Long Do <longdo@Longs-MBP.home>
2022-02-17 16:57:44 +0700

    refactor to extract Eventing functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import axios from 'axios';
 import { User } from './models/User';
-
-// axios.post('http://localhost:3000/users', {
-//   name: 'myname',
-//   age: 20,
-// });
 
 const user = new User({ name: 'new user', age: 30 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
---
+++ src/models/Eventing.ts
@@ -0,0 +1,23 @@
+type Callback = () => void;
+
+export class Eventing {
+  events: { [key: string]: Callback[] } = {};
+
+  on(eventName: string, callBack: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callBack);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callBack) => {
+      callBack();
+    });
+  }
+}
