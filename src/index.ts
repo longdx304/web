c753baf06335be17b976bf6cee8412f9b658b2cc
Long Do <longdo@dobtech.vn>
2022-02-20 23:01:45 +0700

    refactor to delegate passthrough method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
+
+console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.trigger('change');
