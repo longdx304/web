684af7255472766cbc508de52f4e73a83b5293c9
Long Do <longdo@dobtech.vn>
2022-03-10 22:10:11 +0700

    added save front end

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,8 +6,13 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
@@ -29,6 +34,7 @@ export class UserForm extends View<User, UserProps> {
         <input placeholder=${this.model.get('name')} />
         <button class='set-name'>Change Name</button>
         <button class='set-age'>Set Random Age</button>
+        <button class='save-model'>Save User</button>
       </div>
     `;
   }
